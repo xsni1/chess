62f663ccc846ff8f9e331a7ab2b3009645c9e7ed
xsni1 <thesniboy@gmail.com>
2019-02-11 14:13:19 +0100

    Started adding move validation

--- chess.py
+++ chess.py
@@ -17,6 +17,38 @@ class Rook(Piece):
     def __init__(self, pos, team):
         super().__init__(pos, team)
 
+    #check all and append all cells until met ally or enemy or outside of board
+    def possible_moves(self, start_pos, target_pos):
+        valid_moves = []
+        #legal moves "to the right"
+        for x in range(start_pos[1]+1, 8):
+            if board.BOARD[start_pos[0]][x] != '.' and board.BOARD[start_pos[0]][x].team == 'W':
+                break
+            if board.BOARD[start_pos[0]][x] == '.':
+                valid_moves.append([start_pos[0], x])
+        
+        #legal moves "to the left"
+        for x in range(start_pos[1]-1, -1, -1):
+            if board.BOARD[start_pos[0]][x] != '.' and board.BOARD[start_pos[0]][x].team == 'W':
+                break
+            if board.BOARD[start_pos[0]][x] == '.':
+                valid_moves.append([start_pos[0], x])
+
+        #legal moves "to the bottom"
+        for x in range(start_pos[0]+1, 7):
+            if board.BOARD[x][start_pos[1]] != '.' and board.BOARD[x][start_pos[1]].team == 'W':
+                break
+            if board.BOARD[x][start_pos[1]] == '.':
+                valid_moves.append([x, start_pos[1]])
+
+        #legal moves "to the top"
+        for x in range(start_pos[0]-1, -1, -1):
+            if board.BOARD[x][start_pos[1]] != '.' and board.BOARD[x][start_pos[1]].team == 'W':
+                break
+            if board.BOARD[x][start_pos[1]] == '.':
+                valid_moves.append([x, start_pos[1]])
+
+        return valid_moves
 class Board:
     white_pieces = []
     def __init__(self):
@@ -25,9 +57,17 @@ class Board:
     def place(self, pos, piece):
         self.BOARD[pos[0]][pos[1]] = piece
     
-    def initWhites(self):
+    def initWhites(self): #jedna funkcja dla obu druzyn?
         for i in range(8):
             self.white_pieces.append(Pawn([6, i], "W"))
+        self.white_pieces.append(Rook([7, 0], "W"))
+        self.white_pieces.append(Rook([7, 7], "W"))
+
+    def valid_move(self, start_pos, target_pos):
+        print(self.BOARD[start_pos[0]][start_pos[1]].possible_moves(start_pos, target_pos)) #czy nie za poza
+
+ #   def move(self, start_pos, target_pos):
+ #       if valid_move(start_pos, target_pos):
 
     def printBoard(self):
         for i in self.BOARD:
@@ -39,3 +79,4 @@ class Board:
 board = Board()
 board.initWhites()
 board.printBoard()
+board.valid_move([7,0], [7,1])
