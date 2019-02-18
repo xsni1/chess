5492afced857a21b7ec45982d78b8d7c1e4ef84b
xsni1 <thesniboy@gmail.com>
2019-02-18 06:54:26 -0500

    Added black pieces to the board

--- chess.py
+++ chess.py
@@ -5,6 +5,8 @@ class Piece:
         board.place(pos, self)
         
     def __str__(self):
+        if self.team == 'B':
+            return self.symbol.lower()
         return self.symbol
 
 class King(Piece):
@@ -14,7 +16,8 @@ class King(Piece):
     def possible_moves(self, start_pos):
         all_moves = [[start_pos[0]+1, start_pos[1]+1], [start_pos[0], start_pos[1]+1], [start_pos[0], start_pos[1]-1], 
         [start_pos[0]+1, start_pos[1]], [start_pos[0]-1, start_pos[1]], [start_pos[0]-1, start_pos[1]-1], [start_pos[0]-1, start_pos[1]+1], [start_pos[0]+1, start_pos[1]-1]]
-        return [move for move in all_moves if move[0]>=0 and move[0]<=7 and move[1]>=0 and move[1]<=7 and (chessboard[move[0]][move[1]] == '.' or chessboard[move[0]][move[1]].team != chessboard[start_pos[0]][start_pos[1]].team)]
+        return [move for move in all_moves if move[0]>=0 and move[0]<=7 and move[1]>=0 and move[1]<=7 and (chessboard[move[0]][move[1]] == '.' 
+        or chessboard[move[0]][move[1]].team != chessboard[start_pos[0]][start_pos[1]].team)]
 
 class Pawn(Piece):
     symbol = 'P'
@@ -27,13 +30,14 @@ class Pawn(Piece):
         else:
             direction = 1
         attacking_moves = [[start_pos[0] + direction, start_pos[1] - direction], [start_pos[0] + direction, start_pos[1] + direction]]
-        attacking_moves = list(filter(lambda move: move[0]>=0 and move[0]<=7 and move[1] >= 0 and move[1] <= 7 and chessboard[move[0]][move[1]] != '.' and chessboard[move[0]][move[1]].team != chessboard[start_pos[0]][start_pos[1]].team, attacking_moves))
+        attacking_moves = list(filter(lambda move: move[0]>=0 and move[0]<=7 and move[1] >= 0 and move[1] <= 7 and chessboard[move[0]][move[1]] != '.' and 
+        chessboard[move[0]][move[1]].team != chessboard[start_pos[0]][start_pos[1]].team, attacking_moves))
         if not self.moved:
             all_moves = [[start_pos[0] + direction, start_pos[1]], [start_pos[0] + 2*direction, start_pos[1]]]
         else:
             all_moves = [[start_pos[0] + direction, start_pos[1]]]
         self.attacking = False
-        return [move for move in all_moves if move[0]>=0 and move[0] <= 7 and chessboard[move[0]][move[1]] == '.'] + attacking_moves if chessboard[start_pos[0] + direction][start_pos[1]] == '.' else [] #pawn can only move if there is no figure in front of it
+        return [move for move in all_moves if move[0]>=0 and move[0] <= 7 and chessboard[move[0]][move[1]] == '.'] + attacking_moves 
 
 class Knight(Piece):
     symbol = 'N'
@@ -42,7 +46,8 @@ class Knight(Piece):
     
     def possible_moves(self, start_pos): 
         all_moves = [[start_pos[0]-2, start_pos[1]+1], [start_pos[0]-2, start_pos[1]-1], [start_pos[0]+2, start_pos[1]+1], [start_pos[0]+2, start_pos[1]-1]]
-        return [move for move in all_moves if move[0]>=0 and move[0]<=7 and move[1]>=0 and move[1]<=7 and (chessboard[move[0]][move[1]] == '.' or chessboard[move[0]][move[1]].team != chessboard[start_pos[0]][start_pos[1]].team)]
+        return [move for move in all_moves if move[0]>=0 and move[0]<=7 and move[1]>=0 and move[1]<=7 and (chessboard[move[0]][move[1]] == '.' 
+        or chessboard[move[0]][move[1]].team != chessboard[start_pos[0]][start_pos[1]].team)]
 
 class Queen(Piece):
     symbol = 'Q'
@@ -62,27 +67,27 @@ class Bishop(Piece):
         y = start_pos[0]
         #legal moves to the diagonal top right
         for i in range(1, 8):
-            if y-i < 0 or x+i > 7 or (chessboard[y-i][x+i] != '.' and chessboard[y-i][x+i].team == 'W'):
+            if y-i < 0 or x+i > 7 or (chessboard[y-i][x+i] != '.' and chessboard[y-i][x+i].team == chessboard[y][x].team):
                 break
-            if chessboard[y-i][x+i] == '.' or chessboard[y-i][x+i].team == 'B':
+            if chessboard[y-i][x+i] == '.' or chessboard[y-i][x+i].team != chessboard[y][x].team:
                 valid_moves.append([y-i, x+i])
         #legal moves to the diagonal top left
         for i in range(1, 8):
-            if y-i < 0 or x-i < 0 or (chessboard[y-i][x-i] != '.' and chessboard[y-i][x-i].team == 'W'):
+            if y-i < 0 or x-i < 0 or (chessboard[y-i][x-i] != '.' and chessboard[y-i][x-i].team == chessboard[y][x].team):
                 break
-            if chessboard[y-i][x-i] == '.' or chessboard[y-i][x-i].team == 'B':
+            if chessboard[y-i][x-i] == '.' or chessboard[y-i][x-i].team != chessboard[y][x].team:
                 valid_moves.append([y-i, x-i])
         #legal moves to the diagonal bottom left
         for i in range(1, 8):
-            if y+i > 7 or x-i < 0 or (chessboard[y+i][x-i] != '.' and chessboard[y+i][x-i].team == 'W'):
+            if y+i > 7 or x-i < 0 or (chessboard[y+i][x-i] != '.' and chessboard[y+i][x-i].team == chessboard[y][x].team):
                 break
-            if chessboard[y+i][x-i] == '.' or chessboard[y+i][x-i].team == 'B':
+            if chessboard[y+i][x-i] == '.' or chessboard[y+i][x-i].team != chessboard[y][x].team:
                 valid_moves.append([y+i, x-i])
         #legal moves to the diagonal bottom right
         for i in range(1, 8):
-            if y+i > 7 or x+i > 7 or (chessboard[y+i][x+i] != '.' and chessboard[y+i][x+i].team == 'W'):
+            if y+i > 7 or x+i > 7 or (chessboard[y+i][x+i] != '.' and chessboard[y+i][x+i].team == chessboard[y][x].team):
                 break
-            if chessboard[y+i][x+i] == '.' or chessboard[y+i][x+i].team == 'B':
+            if chessboard[y+i][x+i] == '.' or chessboard[y+i][x+i].team != chessboard[y][x].team:
                 valid_moves.append([y+i, x+i])
         return valid_moves
 
@@ -99,30 +104,30 @@ class Rook(Piece):
 
         #legal moves "to the right"
         for x in range(start_pos[1]+1, 8):
-            if board.BOARD[start_pos[0]][x] != '.' and board.BOARD[start_pos[0]][x].team == 'W':
+            if board.BOARD[start_pos[0]][x] != '.' and board.BOARD[start_pos[0]][x].team == board.BOARD[start_pos[0]][start_pos[1]].team:
                 break
-            if board.BOARD[start_pos[0]][x] == '.' or board.BOARD[start_pos[0]][x].team == 'B':
+            if board.BOARD[start_pos[0]][x] == '.' or board.BOARD[start_pos[0]][x].team != board.BOARD[start_pos[0]][start_pos[1]].team:
                 valid_moves.append([start_pos[0], x])
 
         #legal moves "to the left"
         for x in range(start_pos[1]-1, -1, -1):
-            if board.BOARD[start_pos[0]][x] != '.' and board.BOARD[start_pos[0]][x].team == 'W':
+            if board.BOARD[start_pos[0]][x] != '.' and board.BOARD[start_pos[0]][x].team == board.BOARD[start_pos[0]][start_pos[1]].team:
                 break
-            if board.BOARD[start_pos[0]][x] == '.':
+            if board.BOARD[start_pos[0]][x] == '.' or board.BOARD[start_pos[0]][x].team != board.BOARD[start_pos[0]][start_pos[1]].team:
                 valid_moves.append([start_pos[0], x])
 
         #legal moves "to the bottom"
         for x in range(start_pos[0]+1, 8):
-            if board.BOARD[x][start_pos[1]] != '.' and board.BOARD[x][start_pos[1]].team == 'W':
+            if board.BOARD[x][start_pos[1]] != '.' and board.BOARD[x][start_pos[1]].team == board.BOARD[start_pos[0]][start_pos[1]].team:
                 break
-            if board.BOARD[x][start_pos[1]] == '.':
+            if board.BOARD[x][start_pos[1]] == '.' or board.BOARD[x][start_pos[1]].team != board.BOARD[start_pos[0]][start_pos[1]].team:
                 valid_moves.append([x, start_pos[1]])
 
         #legal moves "to the top"
         for x in range(start_pos[0]-1, -1, -1):
-            if board.BOARD[x][start_pos[1]] != '.' and board.BOARD[x][start_pos[1]].team == 'W':
+            if board.BOARD[x][start_pos[1]] != '.' and board.BOARD[x][start_pos[1]].team == board.BOARD[start_pos[0]][start_pos[1]].team:
                 break
-            if board.BOARD[x][start_pos[1]] == '.':
+            if board.BOARD[x][start_pos[1]] == '.' or board.BOARD[x][start_pos[1]].team != board.BOARD[start_pos[0]][start_pos[1]].team:
                 valid_moves.append([x, start_pos[1]])
 
         return valid_moves
@@ -130,23 +135,38 @@ class Rook(Piece):
 
 class Board:
     white_pieces = []
+    black_pieces = []
     def __init__(self):
         self.BOARD = [['.']*8 for i in range(8)]
 
     def place(self, pos, piece):
         self.BOARD[pos[0]][pos[1]] = piece
     
-    def initWhites(self): #jedna funkcja dla obu druzyn?
-        for i in range(8):
-            self.white_pieces.append(Pawn([6, i], 'W'))
-        self.white_pieces.append(Rook([7, 0], 'W'))
-        self.white_pieces.append(Rook([7, 7], 'W'))
-        self.white_pieces.append(Knight([7, 1], 'B'))
-        self.white_pieces.append(Knight([7, 6], 'W'))
-        self.white_pieces.append(Bishop([7, 2], 'W'))
-        self.white_pieces.append(Bishop([7, 5], 'W'))
-        self.white_pieces.append(Queen([7, 3], 'W'))
-        self.white_pieces.append(King([7, 4], 'W'))
+    def initBoard(self): #jedna funkcja dla obu druzyn?
+        def initBlacks():
+            for i in range(8):
+                self.white_pieces.append(Pawn([6, i], 'W'))
+            self.white_pieces.append(Rook([7, 0], 'W'))
+            self.white_pieces.append(Rook([7, 7], 'W'))
+            self.white_pieces.append(Knight([7, 1], 'W'))
+            self.white_pieces.append(Knight([7, 6], 'W'))
+            self.white_pieces.append(Bishop([7, 2], 'W'))
+            self.white_pieces.append(Bishop([7, 5], 'W'))
+            self.white_pieces.append(Queen([7, 3], 'W'))
+            self.white_pieces.append(King([7, 4], 'W'))
+        def initWhites():
+            for i in range(8):
+                self.black_pieces.append(Pawn([1, i], 'B'))
+            self.black_pieces.append(Rook([0, 0], 'B'))
+            self.black_pieces.append(Rook([0, 7], 'B'))
+            self.black_pieces.append(Knight([0, 1], 'B'))
+            self.black_pieces.append(Knight([0, 6], 'B'))
+            self.black_pieces.append(Bishop([0, 2], 'B'))
+            self.black_pieces.append(Bishop([0, 5], 'B'))
+            self.black_pieces.append(Queen([0, 3], 'B'))
+            self.black_pieces.append(King([0, 4], 'B'))
+        initBlacks()
+        initWhites()
 
     def valid_move(self, start_pos, target_pos):
         if target_pos in self.BOARD[start_pos[0]][start_pos[1]].possible_moves(start_pos): #czy nie za poza
@@ -174,13 +194,16 @@ class Board:
             print(i + ' ', end='')
     
 
+
 board = Board()
 chessboard = board.BOARD
-board.initWhites()
+board.initBoard()
+print(board.white_pieces)
 while True:
     board.printBoard()
     print('\nyour move:')
     move = input() # "a3d4" a3 current location d4 target location
     current_pos = [7-int(move[1])+1, ord(move[0])-97]
     target_pos = [7-int(move[3])+1, ord(move[2])-97]
+    print(target_pos + current_pos)
     board.move(current_pos, target_pos)
